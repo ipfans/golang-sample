6eecced5e528b7393bed2c43e866203d52dfbfe1
ipfans <ipfanscn@gmail.com>
2014-01-13 17:41:38 +0800

    add bplist_decoder

---
+++ plistdecoder.go
@@ -0,0 +1,27 @@
+package main
+import (
+    "github.com/DHowett/go-plist"
+    "os"
+    "fmt"
+    "io/ioutil"
+    "bytes"
+)
+func main() {
+    // Can not run with https://github.com/DHowett/go-plist/issues/2
+    if len(os.Args) != 2 {
+        fmt.Println("decoder.exe [Info.plist]")
+        return
+    }
+    filename := os.Args[1]
+    filebuf,err := ioutil.ReadFile(filename)
+    if err != nil{
+        fmt.Println(err)
+        return
+    }
+
+    var bval interface{}
+    reader := bytes.NewReader(filebuf)
+    decoder := plist.NewDecoder(reader)
+    decoder.Decode(bval)
+    fmt.Println(bval)
+}
